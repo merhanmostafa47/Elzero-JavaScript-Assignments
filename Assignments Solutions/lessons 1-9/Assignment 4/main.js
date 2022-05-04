f7630bafa1a0c582d9c7c7b07d8bdc470fcce55f
merhan mostafa <merrymostafa14@gmail.com>
2022-05-04 04:01:01 +0200

    Add week 1 lessons 1 to 9

---
+++ Assignments Solutions/lessons 1-9/Assignment 4/main.js
@@ -0,0 +1,15 @@
+console.group("Group 1");
+console.log("Message one");
+console.log("Message two");
+console.group("Child Group");
+console.log("Message one");
+console.log("Message two");
+console.group("Grand Child Group");
+console.log("Message one");
+console.log("Message two");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group 2");
+console.log("Message one");
+console.log("Message two");
